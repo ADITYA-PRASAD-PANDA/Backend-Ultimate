b5fc5e7d0aeb0d195ae8969a7ceae3f3abaf3019
ADITYA-PRASAD-PANDA <adityaprasadpanda977@gmail.com>
2024-05-22 04:00:24 +0530

    Day3

---
+++ Day3(UnderstandingNPM , Scripts).js
@@ -0,0 +1,21 @@
+// node js waqt jo npm download hota hai use module kaha jata hai . 
+//npm se jo download hota hai use package kaha jata hai . aur koi bhi package download karne ke baad node_modules ke nam pai folder banrga jo ki hame kuch lena dena nahi hai
+
+// package hamara dependencies hai aur dependencies ka dependencies node_modules ke  folder mai hota hai
+
+// devdependencies - aisa packages ko development ke time mein kaam ayega aur jab cloud mein deploy karenge tab ye aur kaam mein nahi ayega
+
+//scripts - bydefault package.json mein scripts ke ander test ke naam par kuch command likha hota hai . npm run karke koi bhi ek naya script run kar sakte hain  
+
+// https://www.npmjs.com/
+
+
+// 1. How to install a package - npm install packagename
+// eg- npm i one-liner-joke  [write these commands in terminal]
+
+// 2 . Uninstall a package - npm uninstall packagename
+// eg- npm uninstall one-liner-joke
+
+// 3.How to install a particular package version - npm install packagename@version
+// eg- npm install accessibility@:5.0.1
+
